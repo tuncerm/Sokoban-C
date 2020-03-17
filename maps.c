f81429d38f6b99183529ecca012af57800e83f10
Mustafa TUNCER <tuncerm@gmail.com>
2020-03-17 15:26:55 +0100

    Added maps and props

---
+++ maps.c
@@ -0,0 +1,182 @@
+const char maps[20][7][10] = {
+    {
+        "HHHHHHHHH\0",
+        "H HO O OH\0",
+        "H      HH\0",
+        "H H     H\0",
+        "HH  OH HH\0",
+        "HHHOO OHH\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "H HH HH H\0",
+        "HOO  HHOH\0",
+        "H H    OH\0",
+        "HOH     H\0",
+        "H HHH  HH\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HHH H H H\0",
+        "HHO   OOH\0",
+        "H OH H OH\0",
+        "HO HO   H\0",
+        "HHOOO   H\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HOOHHO  H\0",
+        "H  H   OH\0",
+        "H O  H  H\0",
+        "H   OO  H\0",
+        "HH O    H\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HH O H  H\0",
+        "H O    OH\0",
+        "H    O  H\0",
+        "HO   H  H\0",
+        "HHOO O OH\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HOO  HOHH\0",
+        "HOO     H\0",
+        "HO H H  H\0",
+        "HH     OH\0",
+        "H H H   H\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HHH H OOH\0",
+        "HHH   O H\0",
+        "HH     OH\0",
+        "HOO  O  H\0",
+        "HHOOO   H\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HHHHHOH H\0",
+        "H O HOO H\0",
+        "H       H\0",
+        "H    H OH\0",
+        "H  H  H H\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HHOO O  H\0",
+        "H O H  OH\0",
+        "H    O  H\0",
+        "HHO  O  H\0",
+        "HH O O  H\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "H HO    H\0",
+        "H HO    H\0",
+        "H H  O  H\0",
+        "HO   O HH\0",
+        "HHHOOO HH\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HHH   O H\0",
+        "H   H   H\0",
+        "HO   H  H\0",
+        "HO     OH\0",
+        "HOOH    H\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "H    HH H\0",
+        "HOH O OHH\0",
+        "HO   H HH\0",
+        "H      HH\0",
+        "H  HHHH H\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "H   OHO H\0",
+        "HOH O  HH\0",
+        "H    O HH\0",
+        "HO    OOH\0",
+        "H  H  H H\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HH    OOH\0",
+        "HOO    OH\0",
+        "HHH  OO H\0",
+        "H H OO  H\0",
+        "HHO  O  H\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HO    OHH\0",
+        "HH      H\0",
+        "HHH  OO H\0",
+        "H H OOO H\0",
+        "HHO  O OH\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HH  H OOH\0",
+        "H  H   HH\0",
+        "H  H    H\0",
+        "H     HHH\0",
+        "H    OHHH\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "HH  H  OH\0",
+        "H  H   OH\0",
+        "H  H   HH\0",
+        "H       H\0",
+        "HH   OH H\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "H  HH HHH\0",
+        "H      HH\0",
+        "H   HOOHH\0",
+        "HH   OOHH\0",
+        "H    O HH\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "H H H  HH\0",
+        "H      HH\0",
+        "H    OOHH\0",
+        "HH  OO HH\0",
+        "HH  HOHHH\0",
+        "HHHHHHHHH\0"
+    },
+    {
+        "HHHHHHHHH\0",
+        "H  HH HHH\0",
+        "H      HH\0",
+        "H   HOOHH\0",
+        "HH  OO HH\0",
+        "HH   O HH\0",
+        "HHHHHHHHH\0"
+    },
+};
